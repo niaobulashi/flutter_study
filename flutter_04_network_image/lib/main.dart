590a19f59e8b6f219c61e5fd96615d70371e4fc2
hulang <hulang6666@qq.com>
2019-06-19 22:21:55 +0800

    添加使用缓存图

--- flutter_04_network_image/lib/main.dart
+++ flutter_04_network_image/lib/main.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
+//import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget{
 
+  // 1、显示来自网上的图片
   @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final title = 'Web Images';
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new Stack(
+          children: <Widget>[
+            new Center(
+              child: new Image.network(
+                'https://img2018.cnblogs.com/blog/1438593/201906/1438593-20190618161447354-729370203.jpg'
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  // 2、用占位符淡入图片
+  /*@override
   Widget build(BuildContext context) {
     final title = 'Fade in Images';
     return new MaterialApp(
@@ -27,5 +54,26 @@ class MyApp extends StatelessWidget{
         ),
       ),
     );
-  }
+  }*/
+
+  //3、使用缓存图片
+  /*@override
+  Widget build(BuildContext context) {
+    final title = 'Cached Images';
+
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new Center(
+          child: new CachedNetworkImage(
+            imageUrl: "http://via.placeholder.com/350x150",
+            placeholder: (context, url) => new CircularProgressIndicator(),
+          ),
+        ),
+      ),
+    );
+  }*/
 }
